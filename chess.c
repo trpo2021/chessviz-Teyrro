18c7cfec9756076e92693ed22a2a320fa340790a
Kirill Voropaev <2vvoropaevv@gmail.com.noreply.github.com>
2021-03-03 01:40:02 +0700

    Add variables for matrix in chess.c file

--- chess.c
+++ chess.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
-#include <iostream>
 #include <string.h>
+
+#include <iostream>
 using namespace std;
 
 int const n = 8;
+int const m = 9;
 
-int main()
-{
-    int i, j;
-    char a[9][8] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                	{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}};
+int main() {
+  int i, j;
+  char a[m][n] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                  {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                  {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                  {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}};
 
-    for (i = 0; i < 9; i++) {
-    	if (i != 8)
-    	{
-    	cout << 8-i << " ";
-    	}
-    	else
-    	{
-			cout << "  ";
-        }
-        for (j = 0; j < n; j++) {
-          printf("%c ", a[i][j]);
-        }
-        printf("\n");
+  for (i = 0; i < m; i++) {
+    if (i != n) {
+      cout << n - i << " ";
+    } else {
+      cout << "  ";
     }
+    for (j = 0; j < n; j++) {
+      printf("%c ", a[i][j]);
+    }
+    printf("\n");
+  }
 }
