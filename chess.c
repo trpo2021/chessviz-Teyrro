f265c6feac45f071af6afb9204d15bec509e708c
Kirill Voropaev <2vvoropaevv@gmail.com.noreply.github.com>
2021-03-14 15:31:16 +0700

    Add size of array with macro

--- chess.c
+++ chess.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <string.h>
-
 #include <iostream>
 using namespace std;
 
-int const n = 8;
-int const m = 9;
+#define numbers_of_string 9
+#define numbers_of_column 8
+
 
 int main() {
   int i, j;
-  char a[m][n] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                  {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                  {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}};
+  char a[numbers_of_string][numbers_of_string] = {
+      {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+      {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+      {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+      {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+      {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}};
 
-  for (i = 0; i < m; i++) {
-    if (i != n) {
-      cout << n - i << " ";
+  for (i = 0; i < numbers_of_string; i++) {
+    if (i != numbers_of_column) {
+      cout << numbers_of_column - i << " ";
     } else {
       cout << "  ";
     }
-    for (j = 0; j < n; j++) {
+    for (j = 0; j < numbers_of_column; j++) {
       printf("%c ", a[i][j]);
     }
     printf("\n");
