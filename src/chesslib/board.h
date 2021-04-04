eedd4e888aae55d8547181c06def38000858cfff
Kirill Voropaev <2vvoropaevv@gmail.com.noreply.github.com>
2021-04-04 16:59:47 +0700

    Add board.h file

--- src/chesslib/board.h
+++ src/chesslib/board.h
@@ -1,4 +1,10 @@
 #pragma once
 #include <string>
+#include <iostream>
 
-bool border_check(string input, const int size_str);
+using namespace std;
+#define numbers_of_string 8
+#define nubmers_of_column 8
+
+void Output_Chessboard(char table[8][8]);
+bool border_check(string input, const int size_str, int x, int x_end);
